e3e97b1de8dc6dae783df8e4abe62bbaf0b5f71e
ddsdharan <ddsdharan@outlook.com>
2025-02-15 19:22:38 +0530

    commits

--- client/src/global.js
+++ client/src/global.js
@@ -1,6 +1,2 @@
 // export const API = "http://localhost:8000";
 export const API = "https://password-reset-server-liue.onrender.com";
-<<<<<<< HEAD
-=======
-
->>>>>>> ccc7a067f1e6f9d76991d558f534c7e864b785ef
